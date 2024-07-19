e291da47ecd686c5937e1d16e578240a63be95d7
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-07-19 16:26:47 +0200

    mobile

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// const { parse } = require("dotenv");
+
 document.addEventListener('DOMContentLoaded', function () {
     const faders = document.querySelectorAll('.fade-in');
-    const h1Anim = document.querySelectorAll('.center'); // Update this line to select the h1 element with the class 'center'
+    const h1Anim = document.querySelectorAll('.center'); 
 
     const appearOptions = {
         threshold: 0,
@@ -26,3 +28,27 @@ document.addEventListener('DOMContentLoaded', function () {
         appearOnScroll.observe(anim);
     });
 });
+
+const slider = document.getElementById('myRange');
+    slider.addEventListener('input', updateTeamName);
+    updateTeamName();
+
+    function updateTeamName() {
+        const teamName = document.getElementById('teamName');
+        const value = parseInt(slider.value, 10);
+
+        if (value >= 500 && value < 1000) {
+            teamName.textContent = "Delta Machines 5";
+        } else if (value >= 1000 && value < 2500) {
+            teamName.textContent = "Delta Machines 4";
+        } else if (value >= 2500 && value < 4000) {
+            teamName.textContent = "Delta Machines 3";
+        } else if (value >= 4000 && value < 5000) {
+            teamName.textContent = 'Delta Machines 2';
+        } else if (value >= 5000) {
+            teamName.textContent = 'Delta Machines 1';
+        } else {
+            teamName.textContent = '';
+        }
+    }
+;
