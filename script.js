8d9d2e6d113f3fa83d4ea8a9b1cac3820fc37d01
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-07-24 20:03:39 +0200

    slider pc done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const faders = document.querySelectorAll('.fade-in');
-    const h1Anim = document.querySelectorAll('.center'); 
+    const h1Anim = document.querySelectorAll('.center');
 
     const appearOptions = {
         threshold: 0,
@@ -30,25 +30,25 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 const slider = document.getElementById('myRange');
-    slider.addEventListener('input', updateTeamName);
-    updateTeamName();
+slider.addEventListener('input', updateTeamName);
+updateTeamName();
 
-    function updateTeamName() {
-        const teamName = document.getElementById('teamName');
-        const value = parseInt(slider.value, 10);
 
-        if (value >= 500 && value < 1000) {
-            teamName.textContent = "Delta Machines 5";
-        } else if (value >= 1000 && value < 2500) {
-            teamName.textContent = "Delta Machines 4";
-        } else if (value >= 2500 && value < 4000) {
-            teamName.textContent = "Delta Machines 3";
-        } else if (value >= 4000 && value < 5000) {
-            teamName.textContent = 'Delta Machines 2';
-        } else if (value >= 5000) {
-            teamName.textContent = 'Delta Machines 1';
-        } else {
-            teamName.textContent = '';
-        }
+function updateTeamName() {
+    const teamName = document.getElementById('teamName');
+    const value = parseInt(slider.value, 10);
+
+    if (value >= 500 && value < 1000) {
+        teamName.textContent = "Delta Machines 5";
+    } else if (value >= 1000 && value < 2500) {
+        teamName.textContent = "Delta Machines 4";
+    } else if (value >= 2500 && value < 4000) {
+        teamName.textContent = "Delta Machines 3";
+    } else if (value >= 4000 && value < 5000) {
+        teamName.textContent = 'Delta Machines 2';
+    } else if (value >= 5000) {
+        teamName.textContent = 'Delta Machines 1';
+    } else {
+        teamName.textContent = '';
     }
-;
+}
